5d806f544cb7e9b6a46763b97ae90918b5515b19
Max <php.academy.max@gmail.com>
2016-08-05 19:21:38 +0300

    second commit

---
+++ header.php
@@ -0,0 +1,5 @@
+<h1>Это Заголовок нашего сайта</h1>
+<a href="/?page=admin">Админка</a>
+<a href="/?page=articles">Статьи</a>
+<a href="/?page=contacts">Контакты</a>
+<a href="/?page=main">Основная</a>
